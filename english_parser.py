824385bf9b6617a4a401e428b44bb695aaf15faf
moju <michaelmoju@gmail.com>
2019-03-08 19:05:54 +0800

    Initial commit

---
+++ english_parser.py
@@ -0,0 +1,12 @@
+import json
+import xml.etree.ElementTree as ET
+
+
+if __name__ == '__main__':
+	import argparse
+
+	DEBUG = 0
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--Data_path', default='./Data/LDC2006T06/data/English/')
+
